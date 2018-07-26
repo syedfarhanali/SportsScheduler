0eb14ad91eecc4604030887f654bfa649b44b482
syedfarhanali <syedfarhanali31@gmail.com>
2018-07-26 15:47:33 +0530

    Initial bootstrapping of project

---
+++ src/main/java/com/sports/SportsScheduler/SportsSchedulerApplication.java
@@ -0,0 +1,12 @@
+package com.sports.SportsScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsSchedulerApplication.class, args);
+	}
+}
